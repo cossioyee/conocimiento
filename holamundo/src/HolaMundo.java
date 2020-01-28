b6a50db5219253f614419aa771833f20171877be
Jean Cossio <cossioyee16@yahoo.es>
2020-01-27 21:45:10 -0500

    subiendo el txt de comandos de docker

---
+++ holamundo/src/HolaMundo.java
@@ -0,0 +1,11 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        NewArraay = 
+        int newArray[99];
+        }
+    }
+
+    public static void print(){
+        System.out.println("....ddddd.......");
+    }
+}
